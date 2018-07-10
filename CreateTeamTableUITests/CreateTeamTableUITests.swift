350255b0e36a1d1a24361436d650217003ee2d5e
Avadh Bambhroliya <avadhbambhroliya.ibl@gmail.com>
2018-07-10 16:15:15 +0530

    remove

--- CreateTeamTableUITests/CreateTeamTableUITests.swift
+++ CreateTeamTableUITests/CreateTeamTableUITests.swift
@@ -24,15 +24,6 @@ class CreateTeamTableUITests: XCTestCase {
     
     func testExample() {
         
-        let tablesQuery = XCUIApplication().tables
-        tablesQuery.children(matching: .cell).element(boundBy: 0).staticTexts["Avadhdd"].swipeRight()
-        
-        let button = tablesQuery.children(matching: .other).matching(identifier: "TEAM NAME*").element(boundBy: 0).buttons["Button"]
-        button.tap()
-        button.tap()
-        button.tap()
-        tablesQuery.children(matching: .other).matching(identifier: "TEAM NAME*").element(boundBy: 1).buttons["Button"].tap()
-        
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
--- lib/xcov/version.rb
+++
@@ -1,6 +0,0 @@
-module Xcov
-
-  VERSION = "1.4.3"
-  DESCRIPTION = "xcov is a friendly visualizer for Xcode's code coverage files"
-
-end
